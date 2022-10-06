e4ae43e779dff21a3311aaecc5ba6779f0fee142
Андрей Шванов <shvanov@mail.ru>
2022-10-06 17:38:13 +0300

    Delete .vscode directory

--- .vscode/loaddata.py
+++
@@ -1,8 +0,0 @@
-import json
-
-from ..recipes.models import Ingredient
-
-with open('d:/dev/foodgram-project_react/.vscode/ingredients.json', encoding='utf-8') as f:
-    data = json.load(f)
-for i in data:
-    Ingredient.objects.create(name=i['name'], measurement_unit=i['measurement_unit'])
