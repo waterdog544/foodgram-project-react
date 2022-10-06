ee620fb1597340721dc1e955b22cd3c2e8722f94
Andrey <shvanov@mail.ru>
2022-10-06 17:26:34 +0300

    ред

--- .vscode/loaddata.py
+++ .vscode/loaddata.py
@@ -1,7 +1,8 @@
 import json
-from recipes.models import Ingredient
 
-with open('ingredients.json', encoding='utf-8') as f:
+from ..recipes.models import Ingredient
+
+with open('d:/dev/foodgram-project_react/.vscode/ingredients.json', encoding='utf-8') as f:
     data = json.load(f)
 for i in data:
     Ingredient.objects.create(name=i['name'], measurement_unit=i['measurement_unit'])
